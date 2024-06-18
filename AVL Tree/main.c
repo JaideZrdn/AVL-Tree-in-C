90da8485b38be405d93d352128322835c41f0400
JaideZrdn <jaide.zardin@icomp.ufam.edu.br>
2024-06-18 14:31:03 -0400

    Initial Commit

---
+++ AVL Tree/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AVL Tree
+//
+//  Created by Jaide Zardin on 18/06/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
